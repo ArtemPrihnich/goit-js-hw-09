cf32d2676b7531781d7bb0f633838d8272844b14
ArtemPrihnich <95173120+ArtemPrihnich@users.noreply.github.com>
2022-07-26 16:21:51 +0000

    Deploying to gh-pages from @ ArtemPrihnich/goit-js-hw-09@ba05644c257c4cfe3e61b36e89fd95345906f336 🚀

---
+++ 01-color-switcher.cb64ebd4.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");let o=null;function r(){t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}e.addEventListener("click",(function(){e.setAttribute("disabled",""),o=setInterval(r,1e3)})),n.addEventListener("click",(function(){e.removeAttribute("disabled"),clearInterval(o)}));
+//# sourceMappingURL=01-color-switcher.cb64ebd4.js.map
