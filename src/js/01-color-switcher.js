ba05644c257c4cfe3e61b36e89fd95345906f336
ArtemPrihnich <artem.prihnich@gmail.com>
2022-07-26 19:21:23 +0300

    дз 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const body = document.querySelector("body")
+const btnStart = document.querySelector("button[data-start]")
+const btnStop = document.querySelector("button[data-stop]")
+
+let timerId = null;
+
+btnStart.addEventListener("click", onBtnStartClick);
+btnStop.addEventListener("click", onBtnStopClick);
+
+function onBtnStartClick() {
+    btnStart.setAttribute("disabled", "");
+    timerId = setInterval(changeBodyColor, 1000)
+}
+
+function onBtnStopClick() {
+    btnStart.removeAttribute("disabled");
+    clearInterval(timerId);
+}
+
+function changeBodyColor() {
+    body.style.backgroundColor = `${getRandomHexColor()}`;
+}
